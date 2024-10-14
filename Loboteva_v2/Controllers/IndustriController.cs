83a1357213f5e13eb9ee0bd5bbf853b5121e3762
SamSanchez710 <samaragomez855@gmail.com>
2024-10-14 14:46:10 -0600

    Admin

--- Loboteva_v2/Controllers/IndustriController.cs
+++ Loboteva_v2/Controllers/IndustriController.cs
@@ -1,11 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Loboteca_v2.Models;
+using System.Linq;
+using Loboteva_v2.Models;
 
 namespace Loboteca1.Controllers
 {
     public class IndustriController : Controller
     {
+        private readonly LobotecaContext _context; // Usamos LobotecaContext en lugar de ApplicationDbContext
+
+        public IndustriController(LobotecaContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Industri()
         {
+            // Filtramos los 6 libros más recientes que pertenecen a la carrera de Industrial
+            var librosRecientes = _context.ELibros
+                .Where(l => l.IdCarrera == 4) // Suponiendo que 1 es el ID de Industrial
+                .OrderByDescending(l => l.Id)
+                .Take(6)
+                .ToList();
+
+            // Obtenemos las 6 revistas más recientes sin filtrar por carrera
+            var revistasRecientes = _context.Revista
+                .OrderByDescending(r => r.Id)
+                .Take(6)
+                .ToList();
+
+            // Pasamos ambos conjuntos de datos a la vista
+            ViewBag.LibrosRecientes = librosRecientes;
+            ViewBag.RevistasRecientes = revistasRecientes;
+
             return View();
         }
     }
---
+++ Loboteva_v2/Models/LibroRevistaViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Loboteca_v2.Models
+{
+    public class ELibro
+    {
+        public int Id { get; set; } // Identificador único del libro
+        public string Titulo { get; set; } // Título del libro
+        public string ImagenUrl { get; set; } // URL de la imagen de la portada del libro
+        public bool Disponible { get; set; } // Indica si el libro está disponible
+        public DateTime FechaDePublicacion { get; set; } // Fecha de publicación del libro
+        public int IdCarrera { get; set; } // Identificador de la carrera a la que pertenece el libro
+    }
+
+    public class Revista
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string ImagenUrl { get; set; }
+        public bool Disponible { get; set; }
+    }
+}
